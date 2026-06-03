91e62bc89c964a4d40e5cb949477ef925c3416be
shimul811898 <shimulislam01799802008@gmail.com>
2026-06-03 12:55:07 +0600

    oder form data show dashboard

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors());
+app.use(express.json());
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -27,6 +28,7 @@ async function run() {
         await client.connect();
         const db = client.db("motobike");
         const bikecollection = db.collection("bikes");
+        const odercollection = db.collection("oders");
 
         app.get("/bikes", async (req, res) => {
             const result = await bikecollection.find().toArray();
@@ -38,12 +40,44 @@ async function run() {
             const result = await bikecollection.findOne({ _id: new ObjectId(id) });
             res.json(result)
         })
-        
-         app.get("/bikes/:id", async (req, res) => {
-            
-         })
 
-
+        app.post("/oders", async (req, res) => {
+            const order = req.body;
+            const result = await odercollection.insertOne(order);
+            res.json(result);
+        })
+        app.get("/oders/:id", async (req, res) => {
+            const { id } = req.params;
+            const result = await odercollection.findOne({ _id: new ObjectId(id) });
+            res.json(result);
+        })
+        app.get("/oders", async (req, res) => {
+            const result = await odercollection.find().toArray();
+            res.json(result);
+        })
+        app.put("/oders/:id", async (req, res) => {
+            const { id } = req.params;
+            const updatedOrder = req.body;
+            delete updatedOrder._id;
+            try {
+                const result = await odercollection.updateOne(
+                    { _id: new ObjectId(id) },
+                    { $set: updatedOrder }
+                );
+                res.json(result);
+            } catch (error) {
+                res.status(500).json({ error: error.message });
+            }
+        })
+        app.delete("/oders/:id", async (req, res) => {
+            const { id } = req.params;
+            try {
+                const result = await odercollection.deleteOne({ _id: new ObjectId(id) });
+                res.json(result);
+            } catch (error) {
+                res.status(500).json({ error: error.message });
+            }
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
